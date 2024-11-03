d820081213bcb4963b65e9b5d82b44b890d253e8
Prabhupada Das <88918431+Prabhu324@users.noreply.github.com>
2024-11-03 18:47:07 +0530

    Create adding_values.sql

---
+++ sql/adding_values.sql
@@ -0,0 +1,3 @@
+-- Insert a sample user with their Bluetooth address
+INSERT INTO attendance (name, bt_address) VALUES ('abc', 'aa:bb:cc:dd:ee:ff');
+--change the bt_address with the bluetooth address of the device whose attendance is to be marked
